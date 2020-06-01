5f078cbb21d63a0b27e6ad7469a6a59267f859eb
DR <caressofsteel@users.noreply.github.com>
2020-05-31 23:52:06 -0400

    Add More Options

--- YouTubeDL.bat
+++ YouTubeDL.bat
@@ -34,6 +34,9 @@ echo.
 	echo [ 4 ] - Video (Choose Format)
 	echo [ 5 ] - Channel (MP4)
 	echo [ 6 ] - Playlist (MP3)
+	echo [ 7 ] - Time Span (MP4)
+	echo [ 8 ] - Video (Proxy, Best, MP4)
+	echo [ 9 ] - Update YouTube-DL
 	echo.
 echo ########################################################################################
 echo.
@@ -45,6 +48,9 @@ if "%menu%"=="3" (goto download_video_best)
 if "%menu%"=="4" (goto download_video_format)
 if "%menu%"=="5" (goto download_channel)
 if "%menu%"=="6" (goto download_playlist)
+if "%menu%"=="7" (goto download_timespan)
+if "%menu%"=="8" (goto download_video_best_proxy)
+if "%menu%"=="9" (goto update)
 if "%menu%"=="" (goto menu)
 
 
@@ -57,7 +63,7 @@ cls
 ECHO [ YouTube-DL Audio ]
 echo.
 set /P url="Enter Audio URL: "
-youtube-dl.exe -ciw -o "Z:\Music\!NewMusic\%%(title)s.%%(ext)s" -x --audio-format mp3 --embed-thumbnail %url%
+youtube-dl.exe -ciw -o "Z:\Music\!NewMusic\%%(title)s.%%(ext)s" -x --audio-format mp3 --rm-cache-dir --embed-thumbnail %url%
 pause
 goto menu
 
@@ -71,7 +77,7 @@ cls
 ECHO [ YouTube-DL Video ]
 echo.
 set /P url="Enter Video URL: "
-youtube-dl.exe -ciw -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4" -o "D:\Downloads\%%(title)s.%%(ext)s" %url%
+youtube-dl.exe --rm-cache-dir -ciw -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4" -o "D:\Downloads\%%(title)s.%%(ext)s" %url%
 pause
 goto menu
 
@@ -85,7 +91,7 @@ cls
 ECHO [ YouTube-DL (4K, MP4) ]
 echo.
 set /P url="Enter Video URL: "
-youtube-dl.exe -ciw --format "bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 -o "D:\Downloads\%%(title)s.%%(ext)s" %url%
+youtube-dl.exe --rm-cache-dir -ciw --format "bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 -o "D:\Downloads\%%(title)s.%%(ext)s" %url%
 pause
 goto menu
 
@@ -103,7 +109,7 @@ youtube-dl.exe -F %url%
 pause
 set /p vformat="Enter Video Code: "
 set /p aformat="Enter Audio Code: "
-youtube-dl.exe -ciw -f "%vformat%+%aformat%" --merge-output-format mp4 -o "D:\Downloads\%%(title)s.%%(ext)s" %url%
+youtube-dl.exe --rm-cache-dir -ciw -f "%vformat%+%aformat%" --download-archive D:\Downloads\downloaded.txt --merge-output-format mp4 -o "D:\Downloads\%%(title)s.%%(ext)s" %url%
 pause
 goto menu
 
@@ -117,7 +123,7 @@ cls
 ECHO [ YouTube-DL Channel (MP4) ]
 echo.
 set /P url="Enter Channel URL: "
-youtube-dl.exe -ciw -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4" -o "D:\Video\Project\YouTubeDL\%%(title)s.%%(ext)s" --download-archive "D:\Video\Project\YouTubeDL\downloaded.txt" -v %url%
+youtube-dl.exe --rm-cache-dir -ciw -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4" -o "D:\Video\Project\YouTubeDL\%%(title)s.%%(ext)s" --download-archive "D:\Video\Project\YouTubeDL\downloaded.txt" -v %url%
 pause
 goto menu
 
@@ -131,6 +137,48 @@ cls
 ECHO [ YouTube-DL Playlist (MP3) ]
 echo.
 set /P url="Enter Playlist ID or URL: "
-youtube-dl -ciw -o "Z:\Music\!Collections\~Incoming\%%(playlist_index)s - %%(title)s.%%(ext)s" -x --audio-format mp3 --embed-thumbnail --yes-playlist "%url%"
+youtube-dl --rm-cache-dir -ciw -o "Z:\Music\!NewMusic\%%(playlist_index)s - %%(title)s.%%(ext)s" -x --audio-format mp3 --embed-thumbnail --yes-playlist "%url%"
+pause
+goto menu
+
+
+REM ########################################################################################
+REM [- 7 -]
+REM ########################################################################################
+
+:download_timespan
+cls
+ECHO [ YouTube-DL TimeSpan (MP4) ]
+echo.
+set /P url="Enter Video URL: "
+
+pause
+goto menu
+
+REM ########################################################################################
+REM [- 8 -]
+REM ########################################################################################
+
+:download_video_best_proxy
+cls
+ECHO [ YouTube-DL (Proxy, 4K, MP4) ]
+echo.
+set /P url="Enter Video URL: "
+youtube-dl.exe --rm-cache-dir -ciw --proxy socks5://10.10.10.1:1080 --format "bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 -o "D:\Downloads\%%(title)s.%%(ext)s" %url%
+pause
+goto menu
+
+
+REM ########################################################################################
+REM [- 9 -]
+REM ########################################################################################
+
+:update
+cls
+ECHO [ YouTube-DL (UPDATE) ]
+echo.
+
+youtube-dl.exe -U
+
 pause
 goto menu
